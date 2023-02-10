5be80146c02243611788a6738bde690c67905aa0
radheshyamrathore <rsrathore002@gmail.com>
2023-02-10 12:17:55 +0530

    add devise controllers users and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "users#index"
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
